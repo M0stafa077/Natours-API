2da0ffbbd54bfec4ef35db645d187c6cd3c3c675
Mostafa Asaad <ma4190243@gmail.com>
2024-10-21 23:38:24 +0300

    Compiled TS files

--- dist/routes/tour.routes.js
+++ dist/routes/tour.routes.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const tour_controller_1 = __importDefault(require("../controllers/tour.controller"));
 const router = (0, express_1.Router)();
+router
+    .route("/top-5-cheapest")
+    .get(tour_controller_1.default.topToursMiddleware, tour_controller_1.default.findAll);
 router.route("/").get(tour_controller_1.default.findAll).post(tour_controller_1.default.createTour);
 router
     .route("/:id")
