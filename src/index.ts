316cd97f3886a40d28d66e9749d869cbf4580c37
Mostafa Asaad <ma4190243@gmail.com>
2024-11-11 23:12:12 +0200

    Updated the server running message

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,7 @@ app.use(globalErrorHandler);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`See: http://localhost:${PORT}`);
 });
 
 process.on("unhandledRejection", (err: Error) => {
